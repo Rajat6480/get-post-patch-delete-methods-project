3b573be3198d37e9788fb31750b2c84d2a7a90dc
Rajat Kumar <rohitnrajat@gmail.com>
2024-05-02 17:23:38 +0530

    get/post/patch/delete with unit testing using chai and mocha

--- middlewares/userLog.js
+++ middlewares/userLog.js
@@ -1,11 +1,17 @@
-const fs = require("fs")
+const fs = require("fs");
 
- function logReqRes(fileName ){
-  return (req,res,next)=>{fs.appendFile(fileName,`${Date.now()} : ${req.method} : ${req.path}\n`,(err,result)=>{
+function logReqRes(fileName) {
+  return (req, res, next) => {
+    fs.appendFile(
+      fileName,
+      `${Date.now()} : ${req.method} : ${req.path}\n`,
+      (err, result) => {
         next();
-  })}
+      }
+    );
+  };
 }
 
 module.exports = {
-    logReqRes,
-}
+  logReqRes,
+};
