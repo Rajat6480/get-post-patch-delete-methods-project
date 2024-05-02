7aeedab38dede7357386b965ed0d5118e2b173ed
Rajat Kumar <rohitnrajat@gmail.com>
2024-05-02 17:13:05 +0530

    Swagger documentation generation and setup.

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const PORT = 7001;
 const {handleConnection} = require("./connection")
 const {logReqRes} = require('./middlewares/userLog')
 
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+
+
+const options = {
+    definition :{
+        openapi : '3.0.0',
+        info : {
+            title : 'Api project for mongodb using Node Js',
+            version : '1.0.0'
+        },
+        servers : [{
+            url : "http://localhost:7001/"
+        }]
+    },
+    apis : ['./routes/*.js']
+}
+
+
+
+const  swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs',swaggerUi.serve , swaggerUi.setup(swaggerSpec))
+
 handleConnection("mongodb://127.0.0.1:27017/remoteStorage").then(()=>console.log("MongoDb Connected..."))
 app.use(express.urlencoded({extended:false}))
 
